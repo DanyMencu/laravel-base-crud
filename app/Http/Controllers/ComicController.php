64abeb5e9f775942317bdae80818236262af4b96
DanyMencu <dany69men@gmail.com>
2022-01-26 20:20:37 +0100

    Add fillable assignment

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -43,13 +43,15 @@ class ComicController extends Controller
 
         $new_comic = new Comic();
 
-        $new_comic->title = $new_data['title'];
+        /* $new_comic->title = $new_data['title'];
         $new_comic->description = $new_data['description'];
         $new_comic->thumb = $new_data['thumb'];
         $new_comic->price = $new_data['price'];
         $new_comic->series = $new_data['series'];
         $new_comic->sale_date = $new_data['sale_date'];
-        $new_comic->type = $new_data['type'];
+        $new_comic->type = $new_data['type']; */
+
+        $new_comic->fill($new_data);
 
         $new_comic->save();
 
